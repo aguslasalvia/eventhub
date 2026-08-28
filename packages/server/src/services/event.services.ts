a1cff2a22ba5bba6bc3ced90509e90273c969798
aguslasalvia <agus.blumenfeld13@gmail.com>
2026-08-28 09:14:07 -0300

    Add EventService.create and use shared TicketType in TicketService

--- packages/server/src/services/event.services.ts
+++ packages/server/src/services/event.services.ts
@@ -1,12 +1,53 @@
 import Event from "src/core/entities/events"
+import { EventCategory, EventState } from "@eventhub/shared"
 import { pool } from "../db/database"
 
 export default class EventService {
+
+  static async create(title: string,
+    description: string,
+    capacity: number | null,
+    organizerId: number,
+    location: string | null,
+    date: Date | null,
+    category: EventCategory,
+  ): Promise<Event> {
+
+    const event = new Event(null, title, description, location, date, capacity, organizerId, EventState.Draft);
+    const [result] = await pool.execute(
+      `
+      INSERT INTO events (title,description,location,date,naxCapacity,category,status organizerId)
+      Values (? ,? ,? ,? ,? ,? ,? ,?);`,
+      [
+        event.Title,
+        event.Description,
+        event.Location,
+        event.Date,
+        event.MaxCapacity,
+        category,
+        event.Status,
+        event.OrganizerId,
+      ]
+    )
+
+    const insertId = (result as any).insertId;
+    return new Event(insertId, title, description, location, date, capacity, organizerId, EventState.Draft);
+  }
+
   static async findById(id: number): Promise<Event | null> {
     const [rows] = await pool.execute("SELECT * FROM events WHERE id = ?", [id])
     const row = (rows as any[])[0]
     return row ? Event.fromRow(row) : null;
   }
+
+  // TODO: 
+  // findAll
+  // findPublished
+  // publish
+  // cancel
+  // delete
+
+
 }
 
 
--- packages/server/src/services/ticket.services.ts
+++ packages/server/src/services/ticket.services.ts
@@ -1,14 +1,7 @@
-import { TicketCategories } from "@eventhub/shared"
+import { TicketType } from "@eventhub/shared"
 
-export interface TicketType {
-  id: number | null;
-  category: TicketCategories;
-  price: number;
-  totalCapacity: number;
-  availableCapacity: number;
-  eventId: number;
-}
+
 
 export default class TicketService {
-  
+
 }
